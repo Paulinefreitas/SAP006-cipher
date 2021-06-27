92db2802ebaa8de3432571fdd99a381c3ad56eed
Pauline <paulinefrts@gmail.com>
2021-06-27 15:07:09 -0300

    first commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
 const cipher = {
-  // ...
+  encode: function (offset, string) {
+    let novaLetra = "";
+    let novoTexto = "";
+    for (let index = 0; index < string.length; index++) {
+      const numeroDaLetra = string[index].charCodeAt();
+      console.log(numeroDaLetra);
+      if (numeroDaLetra >= 65 && numeroDaLetra <= 90) {
+        novaLetra = ((numeroDaLetra - 65 + offset) % 26) + 65;
+      } else if (numeroDaLetra === 32) {
+        novaLetra = numeroDaLetra;
+      }
+      novoTexto = novoTexto + String.fromCharCode(novaLetra);
+    }
+
+    return novoTexto;
+  },
+  decode: function (offset, string) {
+    //return cipher.encode(-offset, string);
+    let novaLetra = "";
+    let novoTexto = "";
+    for (let index = 0; index < string.length; index++) {
+      const numeroDaLetra = string[index].charCodeAt();
+      if (numeroDaLetra >= 65 && numeroDaLetra <= 90) {
+        novaLetra = ((numeroDaLetra - 65 - offset) % 26) + 65;
+      } else if (numeroDaLetra === 32) {
+        novaLetra = numeroDaLetra;
+      }
+      novoTexto = novoTexto + String.fromCharCode(novaLetra);
+    }
+
+    return novoTexto;
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+// Captura de Mensagem a codificar e offset
+const btnCodificar = document.querySelector("#codificar"); // seleciona o botão de codificar
+
+//captura valores, codifica e imprime mensagem
+btnCodificar.onclick = function () {
+  //captura texto a ser codificado
+  let caixaDeTextoDeEntrada =
+    document.getElementById("texto-a-codificar").value;
+  //captura valor de offset
+  let caixaDeOffsetPos = parseInt(
+    document.getElementById("caixa-offset").value
+  );
+  //codifica mensagem
+  let resultado = cipher.encode(
+    caixaDeOffsetPos,
+    caixaDeTextoDeEntrada.toUpperCase()
+  );
+  console.log(caixaDeTextoDeEntrada, caixaDeOffsetPos, resultado);
+  //seleciona caixa para a resposta
+  const resposta = document.getElementById("resposta");
+  //imprime resposta
+  resposta.value = resultado;
+};
+
+//Captura de Mensagem a decodificar e offset
+const btnDecodificar = document.querySelector("#decodificar"); //seleciona o botão de codificar
+
+//captura valores, decodifica e imprime mensagem
+btnDecodificar.onclick = function () {
+  //captura de texto a ser decodificado
+  let caixaDeTextoDeSaida = document.getElementById("resposta").value;
+  //captura valor offset
+  let caixaDeOffsetNeg = parseInt(
+    document.getElementById("caixa-offset").value
+  );
+  //decodifica mensagem
+  let resultado = cipher.decode(
+    caixaDeOffsetNeg,
+    caixaDeTextoDeSaida.toUpperCase()
+  );
+  console.log(caixaDeTextoDeSaida, caixaDeOffsetNeg, resultado);
+
+  //Seleciona caixa para a resposta
+  const resposta = document.getElementById("texto-a-codificar");
+
+  //imprime  resposta
+  resposta.value = resultado;
+};
