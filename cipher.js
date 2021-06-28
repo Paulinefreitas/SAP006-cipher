4cf38ce8d22f8c06f826a7d76e476137607b6495
Pauline Freitas <paulinefrts@gmail.com>
2021-06-28 17:43:27 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,42 @@
+const cipher = {
+  encode: function (offset, string) {
+    if (typeof offset !== "number" || typeof string !== "string") {
+      throw new TypeError();
+    }
+    let novaLetra = "";
+    let novoTexto = "";
+    for (let index = 0; index < string.length; index++) {
+      const numeroDaLetra = string[index].charCodeAt();
+      console.log(numeroDaLetra);
+      if (numeroDaLetra >= 65 && numeroDaLetra <= 90) {
+        novaLetra = ((numeroDaLetra - 65 + offset) % 26) + 65;
+      } else if (numeroDaLetra === 32) {
+        novaLetra = numeroDaLetra;
+      }
+      novoTexto = novoTexto + String.fromCharCode(novaLetra);
+    }
+
+    return novoTexto;
+  },
+  decode: function (offset, string) {
+    if (typeof offset !== "number" || typeof string !== "string") {
+      throw new TypeError();
+    }
+    //return cipher.encode(-offset, string);
+    let novaLetra = "";
+    let novoTexto = "";
+    for (let index = 0; index < string.length; index++) {
+      const numeroDaLetra = string[index].charCodeAt();
+      if (numeroDaLetra >= 65 && numeroDaLetra <= 90) {
+        novaLetra = ((numeroDaLetra - 90 - offset) % 26) + 90;
+      } else if (numeroDaLetra === 32) {
+        novaLetra = numeroDaLetra;
+      }
+      novoTexto = novoTexto + String.fromCharCode(novaLetra);
+    }
+
+    return novoTexto;
+  },
+};
+
+export default cipher;
