4931f083034f124f009158291d98a19bfa6685b3
Pauline Freitas <paulinefrts@gmail.com>
2021-08-22 15:42:07 -0300

    Ajustes pós entrega

--- src/cipher.js
+++ src/cipher.js
@@ -7,7 +7,6 @@ const cipher = {
     let novoTexto = "";
     for (let index = 0; index < string.length; index++) {
       const numeroDaLetra = string[index].charCodeAt();
-      console.log(numeroDaLetra);
       if (numeroDaLetra >= 65 && numeroDaLetra <= 90) {
         novaLetra = ((numeroDaLetra - 65 + offset) % 26) + 65;
       } else if (numeroDaLetra === 32) {
@@ -40,4 +39,4 @@ const cipher = {
 };
 
 export default cipher;
-''
+("");
